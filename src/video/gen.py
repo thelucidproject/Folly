93f66e24f097727482ddd7c927b277cd0a08ef51
Soroush <soroush.333@gmail.com>
2024-08-20 22:45:47 +0000

    demo prototype

--- src/video/gen.py
+++ src/video/gen.py
@@ -1,213 +1,232 @@
-import torch
 import numpy as np
-import librosa
-import cv2
-import subprocess
+import torch
+from PIL import Image
+import random
+import pickle
+from compel import Compel
+from diffusers import AutoPipelineForImage2Image, AutoPipelineForText2Image
+from diffusers.utils import load_image, export_to_video
 from tqdm.auto import tqdm
+import librosa
+from torchvision.io import write_video
+from torchvision.transforms.functional import pil_to_tensor
 
-
-from pydub import AudioSegment
-
-from diffusers import StableDiffusionPipeline
-from diffusers import DPMSolverMultistepScheduler
-
-def get_audio_amplitudes(path, fps, percussive=False):
-    audio, sr = librosa.load(path)
-    length = audio.shape[0] / sr
-    if percussive:
-        _, audio = librosa.effects.hpss(audio, margin=(1.0,2.0))
-        
-    spec_raw = librosa.feature.melspectrogram(y=audio, sr=sr, n_mels=512, hop_length=sr // fps)
-    spec_max = np.amax(spec_raw, axis=0)
-    spec_norm = (spec_max - np.min(spec_max)) / np.ptp(spec_max)
-    return spec_norm, length
-
-def slerp(v0, v1, t, DOT_THRESHOLD=0.9995):
-    dot = torch.sum(v0 * v1) / (torch.norm(v0) * torch.norm(v1))
-    if torch.abs(dot) > DOT_THRESHOLD:
-        v2 = (1 - t) * v0 + t * v1
-    else:
-        theta_0 = torch.acos(dot)
-        sin_theta_0 = torch.sin(theta_0)
-        theta_t = theta_0 * t
-        sin_theta_t = torch.sin(theta_t)
-        s0 = torch.sin(theta_0 - theta_t) / sin_theta_0
-        s1 = sin_theta_t / sin_theta_0
-        v2 = s0 * v0 + s1 * v1
-    return v2
+from .utils import *
 
 
 class VideoGenerator:
-    def __init__(
-        self, 
-        model_name="CompVis/stable-diffusion-v1-4", 
-        width=512, 
-        height=512, 
-        seed=7, 
-        negative_prompt=None
-    ):
-        
-        self.pipeline = StableDiffusionPipeline.from_pretrained(
-            model_name, torch_dtype=torch.float16, safety_checker=None
+    def __init__(self, model_name="SimianLuo/LCM_Dreamshaper_v7", seed=7):
+        self.i2i_pipe = AutoPipelineForImage2Image.from_pretrained(
+            model_name,
+            torch_dtype=torch.float16,
+            safety_checker=None
         ).to("cuda")
-        self.pipeline.scheduler = DPMSolverMultistepScheduler.from_config(self.pipeline.scheduler.config)
-        self.pipeline.enable_xformers_memory_efficient_attention()
+        
+        self.t2i_pipe = AutoPipelineForText2Image.from_pretrained(
+            model_name,
+            vae=self.i2i_pipe.vae,
+            tokenizer=self.i2i_pipe.tokenizer,
+            text_encoder=self.i2i_pipe.text_encoder,
+            unet=self.i2i_pipe.unet,
+            scheduler=self.i2i_pipe.scheduler,
+            torch_dtype=torch.float16,
+            safety_checker=None
+        ).to('cuda')
+        self.i2i_pipe.enable_xformers_memory_efficient_attention()
+        self.i2i_pipe.set_progress_bar_config(disable=True)
+        self.t2i_pipe.enable_xformers_memory_efficient_attention()
+        self.t2i_pipe.set_progress_bar_config(disable=True)
 
-        self.width = width
-        self.height = height
-        self.noise_shape = (1, self.pipeline.unet.config.in_channels, height // 8, width // 8)
-        self.rand_generator = torch.Generator(device='cuda').manual_seed(seed)
+        self.compel = Compel(
+            tokenizer=self.i2i_pipe.tokenizer, 
+            text_encoder=self.i2i_pipe.text_encoder
+        )
+        self.generator = torch.Generator("cuda").manual_seed(seed)
 
-        self.negative_prompt = negative_prompt
+    def _text_to_image(
+        self,
+        prompt, 
+        negative_prompt=None, 
+        num_inference_steps=4, 
+        guidance_scale=10., 
+        width=512, 
+        height=512
+    ):
+        torch.cuda.empty_cache()
+        return self.t2i_pipe(
+            prompt=prompt,
+            negative_prompt=negative_prompt,
+            num_inference_steps=num_inference_steps,
+            guidance_scale=guidance_scale,
+            generator=self.generator,
+            width=width,
+            height=height
+        ).images[0]
 
-    def _refine_prompt(self, text, style):
-        return f'an image of {text} in the style of {style}, '
-
-    def _generate_noise(self, music_segments, music_amps, fps):
-        N = len(music_segments)
-        segment_latents = [
-            torch.randn(
-                self.noise_shape, 
-                generator=self.rand_generator,
-                device='cuda',
-                dtype=torch.float16
+    def _generate_segment_frames(
+        self, 
+        prompt,
+        style,
+        num_frames, 
+        rotate_direction, 
+        zoom_direction,
+        move_direction,
+        zoom_factor,
+        rotate_factor,
+        move_factor,
+        width=512, 
+        height=512,
+        init_frame=None, 
+        negative_prompt=None,
+        num_inference_steps=4,
+        guidance_scale=8.,
+        strength=0.4,
+    ):
+        torch.cuda.empty_cache()
+        prompt = f'a {style} photo of {prompt} with high quality, high details, 4k.'
+        if init_frame is None:
+            init_frame = self._text_to_image(
+                prompt, 
+                negative_prompt, 
+                num_inference_steps, 
+                guidance_scale
             )
-            for _ in range(N + 1)
-        ]
-        num_frames = [int(np.round(s['duration'] * fps)) for s in music_segments]
-        latents = []
-        for i in range(N):
-            noise_a = segment_latents[i]
-            noise_b = segment_latents[i+1]
-            idx = sum(num_frames[:i])
-            T = np.cumsum(music_amps[idx:idx+num_frames[i]])
-            T /= T[-1]
-            T[0] = 0.0
-            for t in T:
-                latents += [slerp(noise_a, noise_b, t)]
-        return torch.cat(latents, dim=0)
-
-    def _generate_text_embedding(self, speech_segments, speech_amps, fps, max_length):
-        speech_segments_aug = [
-            {'start':0, 'end':0, 'duration':0, 'text':speech_segments[0]['text'], 'style':speech_segments[0]['style']}
-        ] + speech_segments + [
-            {'start':max_length, 'end':max_length, 'duration':0, 'text':speech_segments[-1]['text'], 'style':speech_segments[-1]['style']}
-        ]
-
-        num_frames = [int(np.round(s['duration'] * fps)) for s in speech_segments_aug]
-        text_input = self.pipeline.tokenizer(
-            [self._refine_prompt(s['text'], s['style']) for s in speech_segments_aug],
-            padding="max_length",
-            max_length=self.pipeline.tokenizer.model_max_length,
-            truncation=True,
-            return_tensors="pt",
-        )
-        with torch.no_grad():
-            segment_embeddings = self.pipeline.text_encoder(text_input.input_ids.to('cuda'))[0]
-
-        text_embeddings = []
-        for i, seg in enumerate(speech_segments_aug[:-1]):
-            emb_a = segment_embeddings[i]
-            emb_b = segment_embeddings[i+1]
-            text_embeddings += ([emb_a] * num_frames[i])
-            
-            gap_duration = speech_segments_aug[i+1]['start'] - seg['end']
-            gap_frames = int(np.round(gap_duration * fps))
-            idx = len(text_embeddings)
-            T = np.cumsum(speech_amps[idx:idx+gap_frames])
-            if T[-1] > 0.:
-                T /= T[-1]
-            T[0] = 0.0
-            for t in T:
-                text_embeddings += [torch.lerp(emb_a, emb_b, t)]
-        return torch.stack(text_embeddings)
-
+            init_strength = strength
+        init_frame = init_frame.resize((width, height))
+        
+        frames = [init_frame]
+        for _ in range(num_frames):
+            image = frames[-1]
+            image = zoom(
+                image, 
+                factor=zoom_factor, 
+                direction=zoom_direction, 
+                fill_blanks=True
+            )
+            image = move(
+                image, 
+                factor=move_factor, 
+                direction=move_direction, 
+                fill_blanks=True
+            )
+            image = rotate(
+                image, 
+                factor=rotate_factor, 
+                direction=rotate_direction, 
+                fill_blanks=True
+            )
     
-    def _save_video(self, save_path, frames, speech_filepath, music_filepath, duration, fps):
-        ## Save video
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # For MP4
-        video_writer = cv2.VideoWriter(
-            save_path[:-4] + '-video.mp4', fourcc, fps, (self.width, self.height)
+            frames += self.i2i_pipe(
+                prompt=prompt,
+                negative_prompt=negative_prompt,
+                image=image,
+                num_inference_steps=num_inference_steps,
+                guidance_scale=guidance_scale,
+                strength=strength,
+                generator=self.generator
+            ).images
+        return frames
+
+
+    def _encode_image(self, image):
+        x = self.t2i_pipe.image_processor.preprocess(image).to(torch.float16).to('cuda')
+        return self.t2i_pipe.vae.encode(x).latent_dist.sample().cpu().detach()[0]
+    
+    def _decode_image(self, z):
+        image = self.t2i_pipe.vae.decode(
+            z.unsqueeze(0).to('cuda'), 
+            return_dict=False
+        )[0].cpu().detach()
+        return self.t2i_pipe.image_processor.postprocess(image, output_type='pil', do_denormalize=[True])[0]
+    
+    def _interpolate(self, image1, image2, steps=1, linear=True):
+        T = torch.linspace(0.0, 1.0, 2 + steps).to(torch.float16)[1:-1]
+        x = self._encode_image(image1)
+        y = self._encode_image(image2)
+        res = []
+        for t in T:
+            if linear:
+                z = torch.lerp(x, y, t)
+            else:
+                z = slerp(x, y, t)
+            res += [self._decode_image(z)]
+        return res
+    
+    def _upsample(self, frames, scale=1, progress_bar=None):
+        progress_bar = tqdm if progress_bar is None else progress_bar
+        res = []
+        for i, f in enumerate(progress_bar(frames[:-1])):
+            res += [f]
+            res += self._interpolate(f, frames[i+1], scale - 1, linear=False)
+        res += [frames[-1]]
+        return res
+    
+    def save_video(self, frames, audio_path, save_path, fps=20):
+        audio, sr = librosa.load(audio_path)
+        idx = int(len(frames) / fps * sr) + 1
+        audio = torch.tensor(audio[:idx]).unsqueeze(0)
+        
+        write_video(
+            save_path,
+            torch.stack([pil_to_tensor(frame) for frame in frames]).permute(0, 2, 3, 1),
+            fps=fps,
+            audio_array=audio,
+            audio_fps=sr,
+            audio_codec="aac",
+            options={"crf": "10", "pix_fmt": "yuv420p"},
         )
-        for frame in frames:
-            frame = np.array(frame)
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            video_writer.write(frame)
-        video_writer.release()
-
-        ## Save Audio
-        audio1 = AudioSegment.from_file(speech_filepath)[:duration * 1000]
-        audio2 = AudioSegment.from_file(music_filepath)[:duration * 1000]
-        mixed_audio = audio1.overlay(audio2)
-        mixed_audio.export(save_path[:-4] + '-audio.mp3', format="mp3")
-
-        ## Mix video and Audio
-        command = [
-            'ffmpeg',
-            '-y',
-            '-i', save_path[:-4] + '-video.mp4',
-            '-i', save_path[:-4] + '-audio.mp3',
-            '-c:v', 'copy',
-            '-c:a', 'aac',
-            '-strict', 'experimental',
-            '-b:a', '192k',
-            save_path
-        ]
-        try:
-            subprocess.run(command, check=True)
-            print(f"Successfully merged video_file and audio_file into {save_path}")
-        except subprocess.CalledProcessError as e:
-            print(f"An error occurred: {e}")
-
 
     def generate(
         self, 
-        speech_segments,
-        music_segments, 
-        speech_filepath, 
-        music_filepath, 
-        percussive_reactivity=True,
-        batch_size=32, 
-        duration=None, 
-        guidance_scale=7.,
-        num_inference_steps=50,
-        fps=20,
-        save_path=None
+        durations, 
+        style='realistic',
+        generation_fps=2,
+        final_fps=20,
+        width=512, 
+        height=512,
+        strength=0.45,
+        prompts=None, 
+        rotate_directions=None,
+        zoom_directions=None,
+        move_directions=None,
+        zoom_factors=None,
+        rotate_factors=None,
+        move_factors=None,
+        guidance_scale=10.,
+        num_inference_steps=4,
+        negative_prompt='blurry, fuzzy, low quality, chaotic, poor details, dark, sad',
+        save_path=None,
+        progress_bar=None
     ):
-        
-        speech_amps, _ = get_audio_amplitudes(speech_filepath, fps=fps)
-        music_amps, l = get_audio_amplitudes(music_filepath, fps=fps, percussive=percussive_reactivity)
-        print('Audio activities captured')
-
-        noise_latents = self._generate_noise(music_segments, music_amps, fps)
-        text_embeddings = self._generate_text_embedding(speech_segments, speech_amps, fps, max_length=l)
-        print('latent vectors computed')
-
-        frames = []
-        batch_size = 32
-        if duration is None:
-            n = min(text_embeddings.shape[0], noise_latents.shape[0])
-        else:
-            n = duration * fps
-
-        print('starting generation...')
-        self.pipeline.set_progress_bar_config(disable=True)
-        for i in tqdm(range(n // batch_size + 1)):
-            noise = noise_latents[i*batch_size : (i+1)*batch_size]
-            text_emb = text_embeddings[i*batch_size : (i+1)*batch_size]
-            neg_prompt = None if self.negative_prompt is None else [self.negative_prompt]*noise.shape[0]
-            frames += self.pipeline(
-                height=self.height, 
-                width=self.width, 
-                latents=noise, 
-                prompt_embeds=text_emb, 
-                guidance_scale=guidance_scale,
-                negative_prompt=neg_prompt,
+        frames = [None]
+        progress_bar = tqdm if progress_bar is None else progress_bar
+        for i, dur in enumerate(progress_bar(durations)):
+            n = int(np.ceil(generation_fps * dur))
+            seg_frames = self._generate_segment_frames(
+                prompt='' if prompts is None else prompts[i],
+                style=style,
+                num_frames=n, 
+                rotate_direction=random.choice(['pos', 'neg']) if rotate_directions is None else rotate_direction[i], 
+                zoom_direction=random.choice(['in', 'out']) if zoom_directions is None else zoom_directions[i],
+                move_direction=random.choice(['up', 'down', 'left', 'right']) if move_directions is None else move_directions[i],
+                zoom_factor=random.choice([0., 0.006]) if zoom_factors is None else zoom_factors[i],
+                rotate_factor=random.choice([0., 0.003]) if rotate_factors is None else rotate_factors[i],
+                move_factor=random.choice([0., 0.006]) if move_factors is None else move_factors[i],
+                width=width, 
+                height=height,
+                init_frame=frames[-1], 
+                negative_prompt=negative_prompt,
                 num_inference_steps=num_inference_steps,
-            ).images
-            torch.cuda.empty_cache()
-
-        if save_path is not None:
-            self._save_video(save_path, frames, speech_filepath, music_filepath, duration, fps)
-            
+                guidance_scale=guidance_scale,
+                strength=strength,
+            )
+            frames += seg_frames
+        
+        frames = frames[1:]
+        if final_fps > generation_fps:
+            assert final_fps % generation_fps == 0
+            frames = self._upsample(frames, scale=final_fps // generation_fps, progress_bar=progress_bar)
+        if isinstance(save_path, str):
+            pickle.dump(frames, open(save_path, 'wb'))
         return frames
+        
---
+++ src/video/utils.py
@@ -0,0 +1,93 @@
+import numpy as np
+import cv2
+from PIL import Image
+import torch
+
+
+def slerp(v0, v1, t, DOT_THRESHOLD=0.99):
+    dot = torch.sum(v0 * v1 / (torch.norm(v0) * torch.norm(v1)))
+
+    if torch.abs(dot) > DOT_THRESHOLD:
+        v2 = (1 - t) * v0 + t * v1
+    else:
+        theta_0 = torch.acos(dot)
+        sin_theta_0 = torch.sin(theta_0)
+        theta_t = theta_0 * t
+        sin_theta_t = torch.sin(theta_t)
+        s0 = torch.sin(theta_0 - theta_t) / sin_theta_0
+        s1 = sin_theta_t / sin_theta_0
+        v2 = s0 * v0 + s1 * v1
+    return v2
+
+
+def get_mask(image):
+    mask = np.array(image).sum(axis=2)
+    mask[mask > 0] = 255
+    return 255 - mask.astype('uint8')
+
+def zoom(image, factor=0.05, direction='in', fill_blanks=False):
+    if direction == 'in':
+        mask_width = int(factor * image.width / 2)
+        mask_height = int(factor * image.height / 2)
+        if mask_width == 0 and mask_height == 0:
+            return image
+        resized_image = image.resize((image.height - 2 * mask_height, image.width - 2 * mask_width))
+        blank = np.zeros_like(image)
+        blank[mask_width:-mask_width, mask_width:-mask_width] = resized_image
+        resized_image = Image.fromarray(blank)
+        
+    elif direction == 'out':
+        original_width, original_height = image.size
+        new_width = int(original_width * (1 + factor))
+        new_height = int(original_height * (1 + factor))
+        
+        resized_image = image.resize((new_width, new_height), Image.LANCZOS)
+        
+        left = (new_width - original_width) / 2
+        top = (new_height - original_height) / 2
+        right = left + original_width
+        bottom = top + original_height
+        resized_image = resized_image.crop((left, top, right, bottom))
+        
+    if fill_blanks:
+        resized_image = inpaint(resized_image)
+    return resized_image.convert('RGB')
+    
+def rotate(image, factor=0.001, direction='pos', fill_blanks=False):
+    angle = 360 * factor
+    if direction == 'neg':
+        angle = -angle
+    rotated_image = image.rotate(angle, expand=False)
+    if fill_blanks:
+        rotated_image = inpaint(rotated_image)
+    return rotated_image.convert('RGB')
+
+def move(image, factor, direction, fill_blanks=False):
+    temp = np.array(image)
+    if direction == 'up':
+        width = int(factor * image.height)
+        temp = np.pad(temp, ((width, 0), (0, 0), (0, 0)))
+        temp = temp[:image.height]
+    elif direction == 'down':
+        width = int(factor * image.height)
+        temp = np.pad(temp, ((0, width), (0, 0), (0, 0)))
+        temp = temp[-image.height:]
+    elif direction == 'left':
+        width = int(factor * image.width)
+        temp = np.pad(temp, ((0, 0), (width, 0), (0, 0)))
+        temp = temp[:, :image.width]
+    elif direction == 'right':
+        width = int(factor * image.width)
+        temp = np.pad(temp, ((0, 0), (0, width), (0, 0)))
+        temp = temp[:, -image.width:]
+
+    res = Image.fromarray(temp)
+    if fill_blanks:
+        res = inpaint(res)
+    return res.convert('RGB')
+
+def inpaint(image):
+    mask = get_mask(image)
+    img = np.array(image)
+    img = cv2.inpaint(img, mask, 3, cv2.INPAINT_NS)
+    return Image.fromarray(img)
